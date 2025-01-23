832a2c35e61e6344ffb5c8a2bca093cb590f47bf
Ged <120242971+GedK0tlet@users.noreply.github.com>
2025-01-24 00:02:05 +0300

    idiotos

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 from aiogram import Bot, Dispatcher, types
 import asyncio
 
-bot_api="7701233425:AAEYRT6i28mAhoDoyQQMbYE4iIRWJnuI_io"
+
 
 # bot = Bot(token=bot_api)
 # dp = Dispatcher()
@@ -12,11 +12,12 @@ async def main(token, an_message, list_ids):
     # loop = asyncio.get_event_loop()
     bot = Bot(token)
     try:
-        # for list_id in list_ids:
-        await bot.send_message(list_ids[0], an_message)
+        for list_id in list_ids:
+            await bot.send_message(list_id, an_message)
     except Exception as e:
         await bot.send_message(-1002311333309, "EXCEPTION")
-    await bot.close()
+    s = bot.get_sesson()
+    await s.close()
 
 def send_messages(token, an_message, list_ids):
     asyncio.run(main(token, an_message, list_ids))
